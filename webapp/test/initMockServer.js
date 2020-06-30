e39101e9b7f54e77534e3296ce88e0bd5443898e
Florian Vogt <florian.vogt@sap.com>
2020-06-30 16:30:23 +0200

    ref: make the init of the mockserver async

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -1,20 +1,23 @@
 sap.ui.define([
 	"../localService/mockserver"
-], function (mockserver, MessageBox) {
+], function (mockserver) {
 	"use strict";
 
 	var aMockservers = [];
 
 	// initialize the mock server
 	aMockservers.push(mockserver.init());
-	
-	// load configure sap.m with async load
-	sap.ui.getCore().loadLibrary("sap.m", {async: true});
 
 	Promise.all(aMockservers).catch(function (oError) {
-		sap.ui.require(["sap/m/MessageBox"], function(MessageBox){
-			MessageBox.error(oError.message);
-		});
+		var fnShowErrorMessage = function () {
+			return new Promise(function (resolve, reject) {
+				sap.ui.require(["sap/m/MessageBox"], function (MessageBox) {
+					MessageBox.error(oError.message);
+					resolve();
+				}, reject);
+			});
+		}
+		return sap.ui.getCore().loadLibrary("sap.m", { async: true }).then(fnShowErrorMessage);
 	}).finally(function () {
 		// initialize the embedded component on the HTML page
 		sap.ui.require(["sap/ui/core/ComponentSupport"]);
