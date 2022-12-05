c216bedc4f4b19a08654cf9d62b872e3ba16783e
flovogt <flovogt@users.noreply.github.com>
2022-12-05 08:04:46 +0000

    deploy: bb230f5ff87498403548aa2221cdaa08d4b6e5d2

--- resources/jquery-sap.js
+++ resources/jquery-sap.js
@@ -108,9 +108,9 @@ sap.ui.predefine("jquery.sap.strings",["jquery.sap.global","sap/base/strings/cap
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 sap.ui.predefine("jquery.sap.xml",["jquery.sap.global","sap/ui/util/XMLHelper"],function(e,r){"use strict";e.sap.parseXML=r.parse;e.sap.serializeXML=function(e){var a="";if(window.ActiveXObject){a=e.xml;if(a){return a}}if(window.XMLSerializer){return r.serialize(e)}return a};e.sap.isEqualNode=function(r,a){if(r===a){return true}if(!r||!a){return false}if(r.isEqualNode){return r.isEqualNode(a)}if(r.nodeType!=a.nodeType){return false}if(r.nodeValue!=a.nodeValue){return false}if(r.baseName!=a.baseName){return false}if(r.nodeName!=a.nodeName){return false}if(r.nameSpaceURI!=a.nameSpaceURI){return false}if(r.prefix!=a.prefix){return false}if(r.nodeType!=1){return true}if(r.attributes.length!=a.attributes.length){return false}for(var t=0;t<r.attributes.length;t++){if(!e.sap.isEqualNode(r.attributes[t],a.attributes[t])){return false}}if(r.childNodes.length!=a.childNodes.length){return false}for(var t=0;t<r.childNodes.length;t++){if(!e.sap.isEqualNode(r.childNodes[t],a.childNodes[t])){return false}}return true};e.sap.getParseError=r.getParseError;return e});
-sap.ui.requireSync("jquery.sap.sjax");
 sap.ui.requireSync("jquery.sap.xml");
 sap.ui.requireSync("jquery.sap.strings");
+sap.ui.requireSync("jquery.sap.sjax");
 sap.ui.requireSync("jquery.sap.script");
 sap.ui.requireSync("jquery.sap.resources");
 sap.ui.requireSync("jquery.sap.properties");
--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.108.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20221124-1215" }
+		buildinfo : { lastchange : "", buildtime : "20221205-0803" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
