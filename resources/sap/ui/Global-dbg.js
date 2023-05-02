e7273e3ec04c779f3520067d53cf8d393d2b571d
flovogt <flovogt@users.noreply.github.com>
2023-05-02 07:27:21 +0000

    deploy: 5dd3befc407c48401123bc718a6124c0c668ee3c

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.108.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20230424-1424" }
+		buildinfo : { lastchange : "", buildtime : "20230502-0726" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
