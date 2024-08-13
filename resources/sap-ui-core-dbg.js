8e354e26d29c49fb8bd850e13094354c900887be
flovogt <flovogt@users.noreply.github.com>
2024-08-13 09:42:11 +0000

    deploy: c9ff42d1111957c8da263374949205c662f9494f

--- resources/sap-ui-core-dbg.js
+++ resources/sap-ui-core-dbg.js
@@ -3654,5 +3654,5 @@
 }());
 sap.ui.requireSync("sap/ui/core/Core");
 // as this module contains the Core, we ensure that the Core has been booted
-sap.ui.getCore().boot && sap.ui.getCore().boot();
+sap.ui.getCore?.().boot?.();
 //# sourceMappingURL=sap-ui-core-dbg.js.map
